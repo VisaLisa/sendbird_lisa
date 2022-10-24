0eb0f2cce4372e4a96d031e4db276930bf5f9e8b
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2022-10-23 22:44:45 -0500

    add SendbirdApp

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 Purpose of this project is to utilize Sendbird UIKit for React and build an in-app chat with messaging features
 
+Requirements:
+- React
+- Sendbird Chat SDK for Javascript
+
+
+Reference links:
+- https://sendbird.com/docs/uikit/v3/react/introduction/send-first-message 
+
+Steps:
+- Create a Sendbird account
+- Create a Sendbird application on the Sendbird dashboard
+- Create a new user
+
+1. Install UIKit
+2. Implement UIKit to web app
--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import SendbirdApp from "@sendbird/uikit-react/App";
+import "@sendbird/uikit-react/dist/index.css";
+
+// function notifyUser(notificationText = "Thank you for enabling notification") {
+//   if(!("Notification" in window)) {
+//     alert("Browser does not support notifications");
+//   } else if (Notification.permission === "granted"){
+//     const notification = new Notification(notificationText);
+//   } else if (Notification.permission === "denied") {
+//     Notification.requestPermission().then(permission) => {
+//       if (permission === "granted"){
+//         const notification = new Notification(notificationText);
+//       }
+//     }
+//   }
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SendbirdApp
+        // Add the two lines below.
+        appId="A2DD36A0-3749-49D2-8509-3F8566EEB58A" // Specify your Sendbird application ID.
+        userId="leelisa" // Specify your user ID.
+      />
     </div>
   );
 }
